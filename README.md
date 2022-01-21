8f0e7ae6c30e45f4965cf4216f6b1ab5bbc6e191
TheKenetics <TheKenetics@users.noreply.github.com>
2022-01-20 22:48:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# node_editor_view_image_node
+A Blender addon to quickly view images from image nodes in Blender's image viewer.
+
+## Support
+Created for Blender 3.0.0
+
+## Installing
+1. Download this addon as a zip
+2. Inside Blender Preferences > Addons, click Install from File
+3. Select the zip you downloaded, and install
+
+## Using
+Right click image node in node editor > View Image Node
+
+The image will show up in the UV Editor or Image Editor on your screen.
---
+++ __init__.py
@@ -0,0 +1,69 @@
+bl_info = {
+	"name": "View Image Node",
+	"author": "Kenetics",
+	"version": (0, 1),
+	"blender": (3, 0, 0),
+	"location": "Node Editor > Context Menu",
+	"description": "View image node in image editor.",
+	"warning": "",
+	"wiki_url": "",
+	"category": "Node Editor"
+}
+
+import bpy
+from bpy.props import EnumProperty, IntProperty, FloatVectorProperty, BoolProperty, FloatProperty, StringProperty, PointerProperty
+from bpy.types import PropertyGroup, UIList, Operator, Panel, AddonPreferences
+
+
+## Operators
+class VIN_OT_view_image_node(Operator):
+	bl_idname = "vin.view_image_node"
+	bl_label = "View Image Node"
+	bl_options = {'REGISTER'}
+
+	@classmethod
+	def poll(cls, context):
+		return (
+			context.active_object and
+			context.active_object.active_material and
+			context.active_object.active_material.node_tree and
+			context.active_object.active_material.node_tree.nodes.active and
+			context.active_object.active_material.node_tree.nodes.active.image
+		)
+
+	def execute(self, context):
+		node = context.active_object.active_material.node_tree.nodes.active
+		if not (node.type == "TEX_IMAGE" and node.image):
+			return {'CANCELLED'}
+		for area in context.window.screen.areas:
+			if area.type == "IMAGE_EDITOR":
+				area.spaces[0].image = context.active_object.active_material.node_tree.nodes.active.image
+				break
+		return {'FINISHED'}
+
+
+## Append to UI Helper Functions
+def draw_func(self, context):
+	self.layout.operator(VIN_OT_view_image_node.bl_idname, icon="IMAGE_DATA")
+
+## Register
+classes = (
+	VIN_OT_view_image_node,
+)
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+	
+	## Append to UI
+	bpy.types.NODE_MT_context_menu.append(draw_func)
+
+def unregister():
+	## Remove from UI
+	bpy.types.NODE_MT_context_menu.remove(draw_func)
+	
+	for cls in reversed(classes):
+		bpy.utils.unregister_class(cls)
+
+if __name__ == "__main__":
+	register()
